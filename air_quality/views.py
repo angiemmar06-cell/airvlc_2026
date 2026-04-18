f4774c86b549521dfa44f3ecac2d39697df41d2c
angie <angiemmar06@gmail.com>
2026-04-18 20:01:17 +0200

    ajustes de validaciones stations views

--- air_quality/views.py
+++ air_quality/views.py
@@ -2,14 +2,14 @@ from django.http import JsonResponse
 from .models import Measurement, Station
 
 #filtro que solo permite mostrar los contaminantes que se encuentran en la lista ALLOWED_POLLUTANTS, evitando mostrar otros campos que puedan existir en el modelo Measurement pero que no son relevantes para la API. Esto ayuda a mantener la respuesta de la API más limpia y enfocada en los datos de interés.
-ALLOWED_POLLUTANTS = ['no2', 'pm10', 'pm2_5', 'o3', 'so2', 'co', 'no']
+ALLOWED_POLLUTANTS = ['no2', 'pm10', 'pm2_5', 'o3', 'so2', 'co', 'no', 'nox', 'nh3', 'c7h8', 'c6h6', 'c8h10', 'as_ng_m3', 'ni_ng_m3', 'cd_ng_m3', 'pb_ng_m3', 'b_a_p_ng_m3', 'wind_speed', 'wind_direction', 'max_wind_speed', 'temperature', 'relative_humidity', 'pressure', 'solar_radiation', 'precipitation', 'noise']
 
 # Create your views here.
 def stations_geojson(request):
     stations = Station.objects.filter(measurements__isnull=False).distinct().order_by('name')
     data = []
     #measurements_isnull=False --> trae solo las estaciones que tienen mediciones asociadas, evitando estaciones sin datos
-    
+
     for station in stations:
         data.append({
             "name": station.name,
@@ -22,7 +22,7 @@ def stations_geojson(request):
 
 def measurements_geojson(request):
     #1. Leer los parametros de consulta
-    station_name = request.GET.get("station")
+    station_name = request.GET.get("station", "").strip() #strip() --> elimina espacios en blanco al inicio y al final del nombre de la estación
     pollutant = request.GET.get("pollutant")
     
     #2. Validaciones
@@ -35,8 +35,13 @@ def measurements_geojson(request):
     if pollutant not in ALLOWED_POLLUTANTS:
         return JsonResponse({"error": f"Invalid 'pollutant' parameter. Allowed values are: {ALLOWED_POLLUTANTS}"}, status=400)
     
-    #filtrar mediciones
+    if not Station.objects.filter(name=station_name).exists():
+        return JsonResponse({"error": "Station not found"}, status=404)
+    
+    #3. Filtrar mediciones
     measurements = Measurement.objects.filter(station__name=station_name).order_by('measured_at')
+    
+    #construccion de datos para la respuesta JSON
     data = []
     
     for measurement in measurements [:500]:#limita a las primeras 500 mediciones para evitar sobrecargar la respuesta
